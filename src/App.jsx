2217976fed3db52ee6592ee1f4082bff4ab0b05f
John <mark.john6672@gmail.com>
2022-07-07 19:19:48 +0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
 import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Form from "./components/Form";
-import Meme from "./memesData";
-
-function generateMeme() {}
-
+import Memes from "./memesData";
+let newArr = [];
 function App() {
+    function generateImage() {
+        newArr = Memes.data.memes.filter((x) => {
+            return x.box_count === 2;
+        });
+        setRand(Math.floor(Math.random() * newArr.length));
+        setShowImage(true);
+    }
+    const [rand, setRand] = useState(0);
+    const [showImage, setShowImage] = useState(false);
     return (
         <div className="App">
             <Header />
             <div className="m-auto w-[700px]">
-                <Form />
-                <img src={Meme.data.memes[3].url} />
+                <Form
+                    generate={(e) => {
+                        e.preventDefault();
+                        generateImage();
+                    }}
+                />
+                {showImage ? (
+                    <img className="h-[400px] m-auto" src={newArr[rand].url} />
+                ) : null}
             </div>
         </div>
     );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Form() {
+export default function Form({ generate }) {
     return (
-        <div className="flex-col">
+        <div className="flex-col pb-5">
             <form className="py-8">
                 <input
                     className="border-solid border-2 border-gray-900"
@@ -14,7 +14,10 @@ export default function Form() {
                     type="text"
                     placeholder="Bottom Text"
                 />
-                <button className="mt-4 h-[50px] text-white font-bold w-[100%] bg-gradient-to-r from-[#672280] to-[#A626D3]">
+                <button
+                    className="mt-4 h-[50px] text-white font-bold w-[100%] bg-gradient-to-r from-[#672280] to-[#A626D3]"
+                    onClick={generate}
+                >
                     Get a new meme image 🖼️
                 </button>
             </form>
