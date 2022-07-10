bbb59d30b7b67e2678a4219d7df2dc47b5998be1
jmgb27 <mark.john6672@gmail.com>
2022-07-10 14:16:23 +0800

    functional but not responsive

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,17 @@ import Form from "./components/Form";
 import Memes from "./memesData";
 let newArr = [];
 function App() {
+    const [text, setText] = useState({
+        topText: "",
+        bottomText: "",
+    });
+
+    function fillForm(event) {
+        setText((prevData) => {
+            return { ...prevData, [event.target.name]: event.target.value };
+        });
+    }
+
     function generateImage() {
         newArr = Memes.data.memes.filter((x) => {
             return x.box_count === 2;
@@ -18,13 +29,22 @@ function App() {
             <Header />
             <div className="m-auto w-[700px]">
                 <Form
+                    fillForm={fillForm}
                     generate={(e) => {
                         e.preventDefault();
                         generateImage();
                     }}
                 />
                 {showImage ? (
-                    <img className="h-[400px] m-auto" src={newArr[rand].url} />
+                    <div className="relative">
+                        <img
+                            className="w-[100%] m-auto"
+                            src={newArr[rand].url}
+                        />
+
+                        <p className="meme--text top-0">{text.topText}</p>
+                        <p className="meme--text bottom-0">{text.bottomText}</p>
+                    </div>
                 ) : null}
             </div>
         </div>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export default function Form({ generate }) {
+export default function Form({ generate, fillForm }) {
     return (
         <div className="flex-col pb-5">
             <form className="py-8">
@@ -8,11 +8,15 @@ export default function Form({ generate }) {
                     className="border-solid border-2 border-gray-900"
                     type="text"
                     placeholder="Top Text"
+                    name="topText"
+                    onChange={fillForm}
                 />
                 <input
                     className="border-solid border-2 border-gray-900"
                     type="text"
                     placeholder="Bottom Text"
+                    name="bottomText"
+                    onChange={fillForm}
                 />
                 <button
                     className="mt-4 h-[50px] text-white font-bold w-[100%] bg-gradient-to-r from-[#672280] to-[#A626D3]"
